b229cb39a3800d5aa32c9ec000f4c8a55fd5ead4
Max Lupo <art@maxlupo.com>
2023-06-27 08:26:18 -0400

    moved typing functions to class

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 from board import A3, A2, A1, A0, SCK, MISO, MOSI, D10, D5, D6, D3, BUTTON
-from time import sleep
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_debouncer import Button
+from typey import TypeManager
 
 # user button
 button = DigitalInOut(BUTTON)
@@ -27,7 +27,8 @@ STROBE.value = 1
 # DB25 pin num:   p2    p3    p4    p5  p6  p7  p8  p9
 board_pin_list = [D10, MOSI, MISO, SCK, A0, A1, A2, A3]
 # binary data bit: 7    6      5    4    3   2   1   0
-# list is bit 7:0 so that binary chars like '01010' slot easily into sendChar()
+# list is organized bit 7:0 so that short binary chars like '01010' slot easily
+# into the for loop in sendChar()
 
 # initialize pins as digitalio objects
 for num, pin in enumerate(board_pin_list.copy()):
@@ -35,45 +36,7 @@ for num, pin in enumerate(board_pin_list.copy()):
     board_pin_list[num].direction = Direction.OUTPUT
     board_pin_list[num].value = 0
 
-
-def resetPins(pins):
-    for pin in pins:
-        pin.value = 0
-
-
-def sendChar(char, pins, wait=1):
-    data = [bin(binary) for binary in bytearray(char)]
-    binary_data = data[0].replace('b', '')
-    # we reverse the binary data so that 4bit characters load the correct pins
-    for bin_val, pin_val in zip(reversed(binary_data), pins):
-        pin_val.value = int(bin_val)
-    print("char:", char, "-", "binary data ready:", binary_data)
-    sleep(0.0005*wait)
-    print("strobe start")
-    STROBE.value = False
-    sleep(0.001*wait)
-    STROBE.value = True
-    print("strobe end\n")
-    sleep(0.0005*wait)
-    resetPins(pins)
-
-
-def sendText(text, busy_signal, sel_signal, extra_beeps=False, beep_count=5):
-    count = len(text)-1
-    position = 0
-    sleep(0.005)
-    while position <= count:
-        if not busy_signal.value:
-            sleep(0.0005)
-            sendChar(text[position], pins=board_pin_list)
-            position += 1
-            if extra_beeps and (position % beep_count) == 0:
-                sendChar("\a")  # ASCII Bell (BEL)
-                print("beep!")
-        if not sel_signal.value:
-            print("bye!")
-            break
-
+typeWriter = TypeManager(board_pin_list, BUSY, SEL, STROBE)
 
 story = "not a story...yet!\n\n\n"
 folder = "/stories/"
@@ -84,7 +47,7 @@ while True:
         try:
             with open(folder+'2_MERCEDES.txt', 'r') as reader:
                 story = reader.read()
-            sendText(story, busy_signal=BUSY, sel_signal=SEL)
+            typeWriter.sendText(story)
         except OSError:
-            sendText("no file found\n\n\n\n", busy_signal=BUSY, sel_signal=SEL)
+            typeWriter.sendText("no file found\n\n\n\n")
             print("file not found, try again?")
---
+++ typey.py
@@ -0,0 +1,48 @@
+# typey
+from time import sleep
+
+
+class TypeManager:
+    """class to manage data to and from printer"""
+
+    def __init__(self, pins, busy_signal, sel_signal, strobe_signal):
+        self.pins = pins
+        self.busy_signal = busy_signal
+        self.sel_signal = sel_signal
+        self.strobe_signal = strobe_signal
+
+    def resetPins(self):
+        for pin in self.pins:
+            pin.value = 0
+
+    def sendChar(self, char, wait=1):
+        data = [bin(binary) for binary in bytearray(char)]
+        binary_data = data[0].replace('b', '')
+        # we reverse the binary data so that 4bit chars load the correct pins
+        for bin_val, pin_val in zip(reversed(binary_data), self.pins):
+            pin_val.value = int(bin_val)
+        print("char:", char, "-", "binary data ready:", binary_data)
+        sleep(0.0005*wait)
+        print("strobe start")
+        self.strobe_signal.value = False
+        sleep(0.001*wait)
+        self.strobe_signal.value = True
+        print("strobe end\n")
+        sleep(0.0005*wait)
+        self.resetPins()
+
+    def sendText(self, text, extra_beeps=False, beep_count=5):
+        count = len(text)-1
+        position = 0
+        sleep(0.005)
+        while position <= count:
+            if not self.busy_signal.value:
+                sleep(0.0005)
+                self.sendChar(text[position], pins=self.pins)
+                position += 1
+                if extra_beeps and (position % beep_count) == 0:
+                    self.sendChar("\a")  # ASCII Bell (BEL)
+                    print("beep!")
+            if not self.sel_signal.value:
+                print("bye!")
+                break
